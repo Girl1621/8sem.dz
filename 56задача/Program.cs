faace262e1440c6f76fd03ff397e0e74b65174e0
Ksenia <Kseniakuzevanova@gmail.com>
2022-11-28 15:47:19 +0700

    1commit

---
+++ 56задача/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 56: Задайте двумерный массив. Напишите метод, который будет находить строку 
+// с наименьшей суммой элементов.
+
+Console.WriteLine("Введите m - ");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите n - ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+int[,] array = Create2DArray(m, n);
+Print2DArray(array);
+Console.WriteLine();
+int [] result = FindSumElements(array);
+PrintArray(result);
+Console.WriteLine();
+int min = FindIndex(result);
+Console.WriteLine($"{min + 1} строкa с наименьшей суммой элементов.");
+
+
+int[,] Create2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random random = new Random();
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 20);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [] FindSumElements (int [,] array)
+{
+    int [] result = new int [array.GetLength(0)];
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            sum+= array[i, j];
+        }
+       result[i] = sum;
+    }
+    return result;
+}
+
+int FindIndex (int [] array)
+{
+    int min = array[0];
+    int index = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if(min > array[i])
+        {
+            min = array[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array [i]} ");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите метод, который 
+//упорядочит по убыванию элементы каждой строки двумерного массива.
+
+// Console.WriteLine("Введите m - ");
+// bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+// Console.WriteLine("Введите n - ");
+// bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+// int[,] array = Create2DArray(m, n);
+// Print2DArray(array);
+// Console.WriteLine();
+
+// SortElementsInDescendingOrder(array);
+// PrintArray(array);
+
+
+// int[,] Create2DArray(int m, int n)
+// {
+//     int[,] array = new int[m, n];
+//     Random random = new Random();
+//     for (var i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (var j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = random.Next(10, 20);
+//         }
+//     }
+//     return array;
+// }
+
+// void Print2DArray(int[,] array)
+// {
+//     for (var i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (var j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void SortElementsInDescendingOrder(int [,] array)
+// {
+//     for (int k = 0; k < array.GetLength(0); k++)
+//     {
+//         for (int i = 0; i < array.GetLength(1); i++)
+//         {
+//             for (int j = 0; j < array.GetLength(1); j++)
+//             {
+//                 int max = array[k, j];
+//                 if (array[k, i] > max)
+//                 {
+//                     int temp = array[k, i];
+//                     array[k, i] = array[k, j];
+//                     array[k, j] = temp;
+//                 }
+//             }
+//         }
+//     }
+// }
+// void PrintArray(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       Console.Write($"{array[i, j]} ");
+//     }
+//     Console.WriteLine();
+//   }
+// }
